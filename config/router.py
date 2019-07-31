3b6bda54008968fb6defed07bd63c9d0639c7900
MiaLearnsToCode <meroimia@gmail.com>
2019-07-31 14:21:19 +0100

    heroku test

--- config/router.py
+++ config/router.py
@@ -1,6 +1,15 @@
+import os
 from app import app
 from controllers import images, difficulties, users
 
 app.register_blueprint(images.api, url_prefix='/api')
 app.register_blueprint(difficulties.api, url_prefix='/api')
 app.register_blueprint(users.api, url_prefix='/api')
+
+@app.route('/', defaults={'path':''})
+@app.route('/<path:path>')
+
+def catch_all(path):
+    if os.path.isfile('dist/' + path):
+        return app.send_static_file(path)
+    return app.send_static_file('index.html')
